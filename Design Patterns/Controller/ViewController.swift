f437a8cef64d32564f5250b2d936144a34ffd8c2
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-02-23 22:29:40 +0530

    Thank you God for this learning opportunity!

--- Design Patterns/Controller/ViewController.swift
+++ Design Patterns/Controller/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("Thank you God for this learning opportunity!")
         NullObjectMain()
     }
 
