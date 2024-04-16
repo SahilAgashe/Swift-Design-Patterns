bd8bdd7378557a85b1f5e288ff42ff6e8ebd9664
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-04-16 20:27:50 +0530

    LSP

--- Design Patterns/LSP.swift
+++ Design Patterns/LSP.swift
@@ -7,8 +7,56 @@
 
 import Foundation
 
+
+class Rectangle: CustomStringConvertible {
+    var width = 0
+    var height = 0
+    
+    init() {}
+    init(_ width: Int, _ height: Int) {
+        self.width = width
+        self.height = height
+    }
+    
+    var area: Int {
+        width * height
+    }
+    
+    var description: String {
+        "width: \(width), height: \(height), area: \(area)"
+    }
+}
+
+class Square: Rectangle {
+    var side: Int = 0
+    
+    override var width: Int {
+        get { return side }
+        set { side = newValue }
+    }
+    
+    override var height: Int {
+        get { return side }
+        set { side = newValue }
+    }
+    
+    override var area: Int {
+        side * side
+    }
+    
+    override var description: String {
+        "side: \(side), area: \(area)"
+    }
+}
+
 struct LSPLearn {
     static func main() {
+        let rc = Rectangle()
+        setAndMeasure(rc)
+        
+        let sq = Square()
+        setAndMeasure(sq)
+        
         
         let theory = """
         Liskov Substitution Principle =>
@@ -19,3 +67,12 @@ struct LSPLearn {
         print(theory)
     }
 }
+
+fileprivate func setAndMeasure(_ rc: Rectangle) {
+    // setting height and width
+    rc.width = 3
+    rc.height = 4
+    
+    print("Expected area to be 12 but got \(rc.area)")
+}
+
